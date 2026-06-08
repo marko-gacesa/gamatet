8f3f95ed3ad00e036780b2846fca0a294faae55d
Marko Gaćeša <markogacesa@hotmail.com>
2026-06-08 11:58:28 +0200

    show controls in config menu

--- internal/app/menu_config.go
+++ internal/app/menu_config.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 by Marko Gaćeša
+// Copyright (c) 2025, 2026 by Marko Gaćeša
 // Licensed under the GNU GPL v3 or later. See the LICENSE file for details.
 
 package app
@@ -7,7 +7,9 @@ import (
 	"strconv"
 
 	"github.com/marko-gacesa/gamatet/game/setup"
+	"github.com/marko-gacesa/gamatet/internal/config"
 	. "github.com/marko-gacesa/gamatet/internal/i18n"
+	"github.com/marko-gacesa/gamatet/logic/gamepad"
 	"github.com/marko-gacesa/gamatet/logic/menu"
 	"github.com/marko-gacesa/gamatet/logic/screen"
 )
@@ -22,12 +24,33 @@ func (app *App) menuConfig(ctx screen.Context) *menu.Menu {
 
 	for i := range setup.MaxLocalPlayers {
 		var info string
+
 		name := app.cfg.LocalPlayers.Infos[i].Name
-		if name != "" {
-			info = Tf(KeyConfigEditPlayer, i+1, name)
-		} else {
-			info = Tf(KeyConfigEditPlayerNoName, i+1)
+		source := "?"
+		controls := "?"
+
+		switch app.cfg.LocalPlayers.Infos[i].Input.Source {
+		case config.InputSourceKeyboard:
+			source = T(KeyInputSourceKeyboard)
+			controls = app.cfg.LocalPlayers.Infos[i].Input.Keys.String()
+		case config.InputSourceGamepad:
+			gamepadIdx := app.cfg.LocalPlayers.Infos[i].Input.Gamepad
+			if gamepadIdx >= 0 && gamepadIdx < gamepad.Count {
+				source = T(KeyInputSourceGamepad) + " #" + strconv.Itoa(gamepadIdx+1)
+				if gamepad.Gamepads[gamepadIdx].Connected {
+					controls = "\"" + gamepad.Gamepads[gamepadIdx].Name + "\""
+				} else {
+					controls = T(KeyDeviceNotConnected)
+				}
+			}
 		}
+
+		if name != "" {
+			info = Tf(KeyConfigEditPlayer, i+1, name, source, controls)
+		} else {
+			info = Tf(KeyConfigEditPlayerNoName, i+1, source, controls)
+		}
+
 		items = append(items, menu.NewCommand(&app.screenIDNext,
 			route(routeConfigLocalPlayerSetupN+strconv.Itoa(i)),
 			info,
